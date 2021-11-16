42889fa8080c06a92e21ecfb6b22fe586225f3c5
Brian Hsieh <brianh0702@gmail.com>
2021-11-16 12:36:35 -0800

    Finished Demo 11

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: ,
-  authDomain: ,
-  projectId: ,
-  storageBucket: ,
-  messagingSenderId: ,
-  appId: 
+  apiKey: "AIzaSyDwqKQ2zhG9gFHSeJ01cAFTlnpFhM0Z-BQ",
+  authDomain: "comp1800-demo.firebaseapp.com",
+  projectId: "comp1800-demo",
+  storageBucket: "comp1800-demo.appspot.com",
+  messagingSenderId: "627248052487",
+  appId: "1:627248052487:web:9e70f14e192c3f7a956da2",
 };
   
 
